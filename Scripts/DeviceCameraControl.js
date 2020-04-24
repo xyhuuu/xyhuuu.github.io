0bc0be8b3db3b7bc6faee8aeb2af50d1230ade9f
Xiaoyun Hu <xiaoyunhu0812@gmail.com>
2020-04-24 11:24:11 +1000

    Update DeviceCameraControl.js

--- Scripts/DeviceCameraControl.js
+++ Scripts/DeviceCameraControl.js
@@ -1,26 +1,34 @@
 // Set constraints for the video stream
 var constraints = { video: true, audio: true };
+
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger")
+
 // Access the device camera and stream to cameraView
 function cameraStart() {
-    alert("media Devices: " + 'mediaDevices' in navigator);
-    alert("'getUserMedia' in navigator.mediaDevices: " + 'getUserMedia' in navigator.mediaDevices)
-    if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices){ 
-        navigator.mediaDevices
-            .getUserMedia(constraints)
-            .then(function(stream) {
-            track = stream.getTracks()[0];
-            cameraView.srcObject = stream;
-        })
-        .catch(function(error) {
-            console.error("Oops", error);
-        });
-    }
+    navigator.permissions.query({name:'camera'}).then(function(result) {
+        alert("result: " + result.state);
+        if (result.state == 'granted') {
+            // alert("media Devices: " + 'mediaDevices' in navigator);
+            // alert("'getUserMedia' in navigator.mediaDevices: " + 'getUserMedia' in navigator.mediaDevices)
+            if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices){ 
+                navigator.mediaDevices
+                    .getUserMedia(constraints)
+                    .then(function(stream) {
+                    track = stream.getTracks()[0];
+                    cameraView.srcObject = stream;
+                })
+                .catch(function(error) {
+                    console.error("Oops", error);
+                });
+            }
+        }
+    });
 }
+
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
