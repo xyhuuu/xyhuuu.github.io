fababc863b076c437aad379e9de605dbd42f0bf4
Xiaoyun Hu <xiaoyunhu0812@gmail.com>
2020-05-11 17:27:15 +1000

    added image to present 3d orientation

--- Scripts/DeviceOrientationHandler.js
+++ Scripts/DeviceOrientationHandler.js
@@ -2,6 +2,14 @@ function deviceOrientationHandler(tiltLR, tiltFB, dir){
     document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
     document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
     document.getElementById("doDirection").innerHTML = Math.round(dir);
+
+    //Apply the transform to the image
+    var logo = document.getElementById("imgLogo");
+    logo.style.webkitTransform =
+        "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
+    logo.style.MozTransform = "rotate(" + tiltLR + "deg)";
+    logo.style.transform =
+        "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
 }
 
 function requestOrientationPermission() {
