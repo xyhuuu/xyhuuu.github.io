a255b9464f2e1e76139e603887e3422168429e47
Xiaoyun Hu <xiaoyunhu0812@gmail.com>
2020-05-18 10:29:21 +1000

    ss

--- Scripts/draw.js
+++ Scripts/draw.js
@@ -5,16 +5,16 @@ window.onload = function(){
     var svgzy = d3.select("." + "motionzy");
 
     svgxy.append('circle')
-            .attr('cx', 50)
-            .attr('cy', 50)
-            .attr('r', 25)
+            .attr('cx', 100)
+            .attr('cy', 100)
+            .attr('r', 10)
             .attr('stroke', 'black')
             .attr('fill', '#69a3b2')
 
     svgzy.append('circle')
-            .attr('cx', 50)
-            .attr('cy', 50)
-            .attr('r', 25)
+            .attr('cx', 100)
+            .attr('cy', 100)
+            .attr('r', 10)
             .attr('stroke', 'black')
             .attr('fill', '#db0000')
 }
